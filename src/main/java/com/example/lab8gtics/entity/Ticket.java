14e84c01b5e8731f69ffe8c77a775a2195163274
Gil Zanabria <gzanabriaz28@gmail.com>
2023-06-13 21:49:57 -0500

    entidades

--- src/main/java/com/example/lab8gtics/entity/Ticket.java
+++ src/main/java/com/example/lab8gtics/entity/Ticket.java
@@ -1,4 +1,28 @@
 package com.example.lab8gtics.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "ticket")
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name ="idTipoTicket")
+    private Tipo_ticket_evento idTipoTicket;
+
+    @OneToOne
+    @JoinColumn(name ="idUsuario")
+    private Usuario idUsuario;
+
 }
--- src/main/java/com/example/lab8gtics/entity/Tipo_ticket_evento.java
+++ src/main/java/com/example/lab8gtics/entity/Tipo_ticket_evento.java
@@ -1,4 +1,36 @@
 package com.example.lab8gtics.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tipo_ticket_evento")
 public class Tipo_ticket_evento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+
+    @Column(name = "precio")
+    private String precio;
+
+    @Column(name = "cantidad")
+    private Integer cantidad;
+
+    @OneToOne
+    @JoinColumn(name ="idEvento")
+    private Evento idEvento;
+
+
+
+
+
 }
